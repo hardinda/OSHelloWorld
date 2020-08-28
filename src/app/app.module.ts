df284621ed666386e3161c2efc4c33719b2d5289
Derek Hardin <derek.a.hardin@nasa.gov>
2020-08-28 08:20:40 -0400

    changes to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,36 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+/**
+ import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppConfigService } from './services/app-config.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -33,3 +63,5 @@ export function initConfig(appConfig: AppConfigService) {
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+ */
