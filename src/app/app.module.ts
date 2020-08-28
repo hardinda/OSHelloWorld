5935e9e63b939bfdd0aed2815a82517204fb3f41
Derek Hardin <derek.a.hardin@nasa.gov>
2020-08-28 08:32:24 -0400

    changes to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,6 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
-
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-
-
-/**
- import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppConfigService } from './services/app-config.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -56,7 +26,7 @@ export function initConfig(appConfig: AppConfigService) {
   ],
   providers: [{
     provide: APP_INITIALIZER,
-    useFactory: initConfig,
+    useFactory: (configService: AppConfigService) =>   function () { return configService.loadConfig() },
     deps: [AppConfigService],
     multi: true,
   }],
@@ -64,4 +34,6 @@ export function initConfig(appConfig: AppConfigService) {
 })
 export class AppModule { }
 
- */
+
+//useFactory: initConfig,
+//useFactory: (configService: AppConfigurationService) =>   function () { return configService.loadConfiguration() }
